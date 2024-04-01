63b940aee472e2dfcaea7c3e089e3021f5d2d5d4
di597 <di.ruslanova597@gmail.com>
2024-04-02 00:55:07 +0600

    Homework7

---
+++ lesson7/src/modalPokemon/ModalPokemon.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./ModalPokemon.module.sass";
+import close from "../common/img/close.svg";
+
+const Modal = ({ children, handleShow }) => {
+  return (
+    <>
+      <div className={classes.modalWrapper} onClick={handleShow}></div>
+      <div className={classes.modalContent}>
+        <div className={classes.close} onClick={handleShow}>
+          <div className={classes.img_box}>
+            <img src={close} alt="close" />
+          </div>
+        </div>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Modal;
